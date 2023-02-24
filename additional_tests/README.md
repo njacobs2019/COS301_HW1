ced16936d8cf2a0ae11ea0d8c5db7618be939d81
Nicholas Jacobs <nicholas.jacobs@maine.edu>
2023-02-24 18:16:16 +0000

    Additional testing files

---
+++ additional_tests/README.md
@@ -0,0 +1,7 @@
+These are additional test cases for testing PLY Calcx.  
+I wrote the shell script, but not the input and output test cases.
+
+To run the shell script:
+```
+$ ./additional_tests.sh
+```
---
+++ additional_tests/additional_tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Running 101"
+python3 ../calcx.py < in101.txt > output.txt
+diff output.txt out101.txt
+rm output.txt
+
+echo "Running 102"
+python3 ../calcx.py < in102.txt > output.txt
+diff output.txt out102.txt
+rm output.txt
+
+echo "Running 103"
+python3 ../calcx.py < in103.txt > output.txt
+diff output.txt out103.txt
+rm output.txt
+
+echo "Finished all tests"
